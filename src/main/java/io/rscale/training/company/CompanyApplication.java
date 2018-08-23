1887370d3a4ce6fa2f51dae10bf3f428a869ce57
Ben.Stone <ben.stone@cgi.com>
2018-08-23 16:23:29 -0600

    Revert "all the updates"
    This reverts commit 5deff8dd84789344c8f8bc3de2940d31a1ba6259.

--- src/main/java/io/rscale/training/company/CompanyApplication.java
+++ src/main/java/io/rscale/training/company/CompanyApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CompanyApplication {
 
 	public static void main(String[] args) {
-	    System.out.println("TEST LINE");
-	    SpringApplication.run(CompanyApplication.class, args);
+		SpringApplication.run(CompanyApplication.class, args);
 	}
 }
